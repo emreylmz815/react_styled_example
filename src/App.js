bab1b238c1731eb1b098f9e811c2892959156072
Emre Yılmaz <emreylmz815@gmail.com>
2022-04-12 04:20:04 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="morphing">
+    <div className="word">Hi</div>
+    <div className="word">My</div>
+    <div className="word">Name</div>
+    <div className="word">Is</div>
+    <div className="word">Emre</div>
+    <div className="word">Yılmaz</div>
+    <div className="word">#StayTuned</div>
+</div>
   );
 }
 
